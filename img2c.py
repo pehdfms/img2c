f59b035a5b2a8e62e646812194e79b276e3212bf
Pedro Henrique <pehdfms@gmail.com>
2021-05-28 11:04:32 -0300

    add dimensions to output

--- img2c.py
+++ img2c.py
@@ -10,15 +10,15 @@ def convert_rgb565(pixel):
     return hex(rgb)
 
 def create_matrix(img):
-    x, y, _ = img.shape
+    altura, largura, _ = img.shape
 
     colors = set()
     matrix_str = "static unsigned int sprite[] = {\n"
 
     byte_count = 0
-    for i in range(x):
+    for i in range(altura):
         matrix_str += "    "
-        for j in range(y):
+        for j in range(largura):
             current_color = convert_rgb565(img[i,j])
 
             colors.add(current_color)
@@ -39,8 +39,10 @@ def create_matrix(img):
         colors_str += f"#define C{count} {color}\n"
         matrix_str = matrix_str.replace(color, f"C{count}")
         count += 1
+    colors_str += "\n"
+    dimensions_str = f"#define W {largura}\n#define H {altura}\n"
 
-    return colors_str + "\n" + matrix_str
+    return colors_str + dimensions_str + matrix_str
 
 with open("output.txt", "w") as f:
     results = create_matrix(img)
